c4d9aefa210532c4515c437550e9268c1888dbd9
Ascend Ho <coderhs@foxmail.com>
2023-03-28 16:13:17 +0800

    💡 try built-in queue in c++&java

---
+++ Queues/java/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+//        built-in ArrayDeque
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        var front = queue.remove();
+        System.out.println(front);
+        System.out.println(queue);
+
+
+    }
+
+}
